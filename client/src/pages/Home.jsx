76e0a4570db262571599f15dabb53e69e74431d1
dinna21 <ransikadinura@gmail.com>
2025-07-02 18:40:07 +0530

    I have done some parts navbar and header

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+import KoiVarieties from '../components/KoiVarieties.jsx'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <KoiVarieties />
+    </>
+  )
+}
