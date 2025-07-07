c3faa6c188655d3ae4981b570ee83fb64dc0d38c
dinna21 <ransikadinura@gmail.com>
2025-07-07 23:50:17 +0530

    Add the Shop page to App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Shop from './pages/Shop.jsx'
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />}/>
       </Routes>
     </div>
   )
