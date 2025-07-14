e05e03e0a5338c94964ee5b4d2a0de6957887730
dinna21 <ransikadinura@gmail.com>
2025-07-14 11:21:14 +0530

    add the navbar to all the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Shop from './pages/Shop.jsx'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Shop from './pages/Shop.jsx';
+import Layout from './components/Layouts.jsx';
 
 export default function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />}/>
-      </Routes>
-    </div>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route
+        path="/shop"
+        element={
+          <Layout>
+            <Shop />
+          </Layout>
+        }
+      />
+    </Routes>
+  );
 }
