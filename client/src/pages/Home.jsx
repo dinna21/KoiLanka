08379054cd4f006175a4d076156ae535626d373d
Dinura Ransika <ransikadinura@gmail.com>
2026-03-22 14:22:40 +0530

    feat: implement scrollbar hiding on Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Navbar from '../components/Navbar.jsx'
+import React, { useEffect } from 'react'
 import Header from '../components/Header.jsx'
 import KoiVarieties from '../components/KoiVarieties.jsx'
 import WhyChooseUs from '../components/WhyChooseUs.jsx'
@@ -7,9 +6,18 @@ import HomeServices from '../components/HomeServices.jsx'
 import Footer from '../components/Footer.jsx'
 
 export default function Home() {
+  useEffect(() => {
+    document.documentElement.classList.add('hide-scrollbar-page')
+    document.body.classList.add('hide-scrollbar-page')
+
+    return () => {
+      document.documentElement.classList.remove('hide-scrollbar-page')
+      document.body.classList.remove('hide-scrollbar-page')
+    }
+  }, [])
+
   return (
     <>
-      <Navbar />
       <Header />
       <KoiVarieties />
       <WhyChooseUs />
